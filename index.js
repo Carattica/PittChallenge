f1de67386896fa07083a9053a99be911d3828f84
Caralyn Harben <carattica@cl2-wifi-10-215-50-179.wireless.pitt.edu>
2019-10-12 16:07:57 -0400

    Fixed issue in index.js preventing Actions on Google from recognizing Google libraries

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-/*
-const App = require('actions-on-google').DialogFlowApp;
+
 const functions = require('firebase-functions');
+const {dialogflow} = require('actions-on-google');
+const app = dialogflow({debug: true});
+//console.log(app + "AAAAAAAAAAAAAAAAAAAaaaa");
 
 const fs = require('fs');
 const readline = require('readline');
@@ -11,183 +13,15 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = '{"installed":{"client_id":"309787760900-e8f40nt7qfhosfmkikc4dn9913vlpp6r.apps.googleusercontent.com","project_id":"quickstart-1570863069224","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"L3xZZ1twamOYOf_J0o65miXb","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}';
+var credentials = '{"installed":{"client_id":"309787760900-e8f40nt7qfhosfmkikc4dn9913vlpp6r.apps.googleusercontent.com","project_id":"quickstart-1570863069224","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"L3xZZ1twamOYOf_J0o65miXb","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}';
 
+app.intent('Make appointment', (conv, {$Specialties}) => {
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+  //if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-
- /*
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}*/
-
-/*
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credientials.installed.redirect_uris[0];
-
-  var auth = new googleAuth();
-
-  var oauth2Client = new oAuth2Client(clientId, clientSecret, redirectUrl);
-
-  fs.readFile(TOKEN_PATH, functino(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
-}*/
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-
- /*
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-function storeToken(token) {
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-}
-
-function createEvent(auth) {
-  let userDate = _app.getArgument('date');
-  let eventName = _app.getArgument('name');
-
-  var event = {
-    'summary': 'Appointment with Dr. Pitt',
-    'description': 'Hopefully, we can slap the user\'s input here',
-    'start': {
-    },
-    'end': {
-    }
-  };
-
-  event.summary = eventName;
-  event.start.date = userDate;
-  event.end.date = userDate;
-
-  calendar.events.insert({
-    auth: auth,
-    calendarId: 'primary',
-    resource: event,
-  }, function (err, event) {
-    if (err) {
-      _app.tell('There was an error contacting the Calendar service: ' + err);
-      return;
-    }
-
-    _app.ask(eventName + " has been scheduled for " + userDate + ".");
-  });
-}
-
-function bookAppointment(app) {
-  googleRequest(app, createEvent);
-}
-
-app.intent('Make appointment', (conv, {Specialties}) => {
-  bookAppointment(app);
-});
-
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-
- /*
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
-}*/
-
-
-const app = require('actions-on-google').DialogFlowApp;
-console.log(app);
-
-const functions = require('firebase-functions');
-
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(credentials), createEvent);
 });
 
 /**
@@ -214,9 +48,9 @@ function authorize(credentials, callback) {
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
-  var redirectUrl = credientials.installed.redirect_uris[0];
+  var redirectUrl = credentials.installed.redirect_uris[0];
 
-  var auth = new googleAuth();
+  //var auth = new googleAuth();
 
   var oauth2Client = new oAuth2Client(clientId, clientSecret, redirectUrl);
 
@@ -265,10 +99,9 @@ function storeToken(token) {
   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
 }
 
-function createEvent(auth) {
+function createEvent(oAuth2Client) {
   let userDate = _app.getArgument('date');
   let eventName = _app.getArgument('name');
-  console.log(app);
 
   var event = {
     'summary': 'Appointment with Dr. Pitt',
@@ -301,9 +134,8 @@ function bookAppointment(app) {
   googleRequest(app, createEvent);
 }
 
-app.intent('Make appointment', (conv, {$Specialties}) => {
+  conv.close("Okay, your appointment is set." );
 
-  bookAppointment(app);
 });
 
 /**
@@ -334,3 +166,5 @@ function listEvents(auth) {
     }
   });
 }*/
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
